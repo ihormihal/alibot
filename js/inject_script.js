7fee3d1a9338221b821c4f774ba5e01f62136752
ihormihal <imykhalchenko@jayadigital.com>
2014-11-05 19:21:08 +0200

    fix

--- js/inject_script.js
+++ js/inject_script.js
@@ -13,5 +13,17 @@ for (var i = 0; i < helperSKU.length; i++) {
 	skuVariants+='<option value="'+helperSKU[i].skuAttr+'">'+helperSKU[i].skuAttr+'</option>';
 }
 skuVariants += '</select>';
-var container = '<div id="fast-deals-helper"><div class="helper-form-group"><label>Select SKU:</label>'+skuVariants+'</div></div>';
-$('body').append($(container));
+
+var helperBuyButton = '<a href="" id="helper-buy-button">Buy!</a>';
+var helperContainer = '<div id="fast-deals-helper"><div class="helper-form-group"><label>Select SKU:</label>'+skuVariants+'</div>'+helperBuyButton+'</div>';
+
+
+appendHtml(document.body, helperContainer);
+
+function appendHtml(el, str) {
+  var div = document.createElement('div');
+  div.innerHTML = str;
+  while (div.children.length > 0) {
+    el.appendChild(div.children[0]);
+  }
+}
