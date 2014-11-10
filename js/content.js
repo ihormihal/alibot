015b93d1abeda7d9038efc2a4e7f701d8caa829c
Ihor Mykhalchenko <imykhalchenko@jayadigital.com>
2014-11-11 01:26:38 +0200

    final

--- js/content.js
+++ js/content.js
@@ -1,4 +1,4 @@
-//http://m.aliexpress.com/group/255963063-2052686069-detail.html?fromApp=true&promotionType=GagaGroup&lang=ru
+//inject script
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('js/inject_script.js');
 s.onload = function() {
@@ -6,15 +6,8 @@ s.onload = function() {
 };
 (document.head||document.documentElement).appendChild(s);
 
-document.formLoaded = function(){
-	console.log('formLoaded');
-}
-
-
-
-
-
-
+//main app
+//http://m.aliexpress.com/group/255963063-2052686069-detail.html?fromApp=true&promotionType=GagaGroup&lang=ru
 function pingTime() {
 	var pingStart = new Date().getTime();
 	var helperRequest = new XMLHttpRequest;
@@ -27,18 +20,31 @@ function pingTime() {
 	}
 }
 
-
+var getParams = {
+	'objectId': '',
+	'promotionId': '',
+	'quantity': 1,
+	'countryCode': 'US',
+	'skuAttr': ''
+};
+var params1 = {'fromApp':'true','promotionType':'GagaGroup'};
 document.body.addEventListener('formLoaded', function(e) {
-    var dealTime = document.getElementById('fast-deals-helper').getAttribute('data-dealtime');
+
+	getParams.objectId = e.detail.objectId;
+	getParams.promotionId = e.detail.promotionId;
+	getParams.skuAttr = e.detail.skuAttr;
+
+    var dealTime = e.detail.dealTime;
     var server = pingTime();
 	var timeToDeal = dealTime - server.time;
-	var timeInterval = timeToDeal;
+	var timeInterval = 7;
 
 	//Пингуем в течении N(5) секунд (до начала за timeInterval секунд)
 	var synchroTimePre = 0;
 	var tempPre = 0;
 	var lastPingTime = 0;
-	while(timeToDeal <= timeInterval && timeToDeal > (timeInterval-5)){
+
+	while(timeToDeal <= timeInterval && timeToDeal > (timeInterval-3)){
 		server = pingTime();
 		timeToDeal = dealTime - server.time;
 
@@ -56,11 +62,71 @@ document.body.addEventListener('formLoaded', function(e) {
 
 	//Обновленный, синхронизированный таймер, получаем актуальное время старта относительно локальных часов
 	timeToDeal = (dealTime*1000 - new Date().getTime()) - synchroTime - lastPingTime;
-	console.log(timeToDeal);
 
-	setTimeout(startBuy,timeToDeal)
+	setTimeout(startBuy,timeToDeal);
+	document.body.addEventListener('skuChanged', function(e) {
+		getParams.skuAttr = e.detail;
+	});
+	document.getElementById('helper-buy-button').addEventListener('click', function(e) {
+		e.preventDefault;
+		startBuy(params1);
+	});
 });
 
-function startBuy (){
+
+
+//var HelperGetUrl = 'http://gaga.aliexpress.com/order/confirm_order.htm?objectId='+helperPID+'&promotionId='+helperCID+'&quantity=1&countryCode=US&skuAttr='+skuAttr;
+
+function startBuy1 (params){
 	//Основной запрос для покупки
+	var prm = '';
+	for(i in params){
+		prm = prm+i+'='+params[i]+'&';
+	}
+	var orderRequest = new XMLHttpRequest;
+	orderRequest.open('GET', 'http://m.aliexpress.com/group/255963063-2052686069-detail.html?'+prm, false);
+	orderRequest.send(null);
+	if(orderRequest.status == 200) {
+		var response = orderRequest.responseText;
+		if(response.indexOf('sku-dialog') !== -1){
+			var parser = new DOMParser();
+			var doc = parser.parseFromString(response, "text/html");
+			var form = doc.getElementById('sku-dialog');
+			form.style.display = 'block';
+			document.body.innerHTML = form.outerHTML;
+			document.getElementById('captcha-input').focus();
+		}else{
+			console.log('no form');
+		}
+	}
+}
+
+
+//childNodes
+//lastChild 
+//children 
+//lastElementChild 
+
+function startBuy (params){
+	//Основной запрос для покупки
+	var prm = '';
+	for(i in params){
+		prm = prm+i+'='+params[i]+'&';
+	}
+	var orderRequest = new XMLHttpRequest;
+	orderRequest.open('GET', 'http://gaga.aliexpress.com/order/confirm_order.htm?'+prm, false);
+	orderRequest.send(null);
+	if(orderRequest.status == 200) {
+		var response = orderRequest.responseText;
+		if(response.indexOf('place-order-form') !== -1){
+			var parser = new DOMParser();
+			var doc = parser.parseFromString(response, "text/html");
+			var form = doc.getElementById('place-order-form');
+			form.style.display = 'block';
+			document.body.innerHTML = form.outerHTML;
+			document.getElementById('captcha-input').focus();
+		}else{
+			console.log('no form');
+		}
+	}
 }
--- js/inject_script.js
+++ js/inject_script.js
@@ -1,6 +1,4 @@
 //http://m.aliexpress.com/group/255963063-2052686069-detail.html?fromApp=true&promotionType=GagaGroup&lang=ru;
-var formLoaded = new CustomEvent("formLoaded", {});
-
 var noscript = document.getElementsByTagName('noscript')[0];
 var imgStr = noscript.innerHTML;
 var timeStr = imgStr.substring(imgStr.indexOf('time=')+5,imgStr.indexOf('time=')+15);
@@ -15,26 +13,37 @@ var startafter = helperRemain.h*3600+helperRemain.m*60+helperRemain.s;
 
 var dealTime = currentTime + startafter;
 
-var skuVariants = '<div class="helper-form-group"><label>Select SKU:</label><select name="skuAttr">';
+var skuVariants = '<div class="helper-form-group"><label>Select SKU:</label><select onchange="skuChanged(this);" name="skuAttr">';
 for (var i = 0; i < helperSKU.length; i++) {
 	skuVariants+='<option value="'+helperSKU[i].skuAttr+'">'+helperSKU[i].skuAttr+'</option>';
 }
 skuVariants += '</select></div>';
 
-var helperFormPID = '<input name="objectId" type="hidden" value="'+helperPID+'">';
-var helperFormCID = '<input name="promotionId" type="hidden" value="'+helperCID+'">';
-var helperFormAPP = '<input name="fromApp" type="hidden" value="true">';
-var helperFormQ = '<input name="quantity" type="hidden" value="1">';
-var helperFormC = '<input name="countryCode" type="hidden" value="US">';
-var helperFormB = '<button type="submit" id="helper-buy-button">Buy!</button>';
+var helperFormB = '<button id="helper-buy-button">Buy!</button>';
+
+var helperParams = {
+	'dealTime' : dealTime,
+	'objectId': helperPID,
+	'promotionId': helperCID,
+	'quantity': 1,
+	'countryCode': 'US',
+	'skuAttr': helperSKU[0].skuAttr
+};
+
+var formLoaded = new CustomEvent("formLoaded",{
+	detail: helperParams
+});
+
+function skuChanged(el){
+	var skuChanged = new CustomEvent("skuChanged",{
+		detail: el.value
+	});
+	document.body.dispatchEvent(skuChanged);
+}
+
 
 //PC variant
-//m.aliexpress.com/group/order/confirm_order.htm?objectId=2041880721&promotionId=255963063&quantity=1&countryCode=US&skuAttr=200000828%3A200003982%23X98+Air+3G
-appendHtml(document.body, '<div id="fast-deals-helper" data-dealtime="'+dealTime+'"><form method="get" action="http://group.aliexpress.com/order/confirm_order.htm">'+helperFormPID+helperFormCID+helperFormQ+helperFormAPP+helperFormC+skuVariants+helperFormB+'</form></div>');
-//APP variant
-//gaga.aliexpress.com/order/orderConfirm?q=1&productId=2052686069&skuAttr=200000828:200003982
-//appendHtml(document.body, '<div id="fast-deals-helper"><form method="get" action="http://group.aliexpress.com/order/confirm_order.htm">'+helperFormPID+helperFormCID+helperFormQ+helperFormAPP+helperFormC+skuVariants+helperFormB'</form></div>');
-
+appendHtml(document.body, '<div id="fast-deals-helper">'+skuVariants+helperFormB+'</div>');
 
 function appendHtml(el, str) {
   var div = document.createElement('div');
